74f50bcf2821bb13ff6ec653a0aa4e05fb36f669
Anthony Grimes <i@raynes.me>
2014-05-05 14:27:27 -0700

    Update for clojure 1.6.0 and clean up a bit.

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject clj-yaml "0.4.1"
   :description "YAML encoding and decoding for Clojure using SnakeYAML"
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :url "http://github.com/lancepantz/clj-yaml"
-  :dependencies
-    [[org.clojure/clojure "1.2.0"]
-     [org.yaml/snakeyaml "1.11"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.yaml/snakeyaml "1.11"]])
